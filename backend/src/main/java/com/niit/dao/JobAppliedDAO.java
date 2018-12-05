e335b821cdea12116274d8b098ab0fea3e6460cc
vidhyalakshmi123 <41991820+vidhyalakshmi123@users.noreply.github.com>
2018-12-05 15:33:52 +0530

    Add files via upload

---
+++ backend/src/main/java/com/niit/dao/JobAppliedDAO.java
@@ -0,0 +1,16 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.model.JobApplied;
+
+public interface JobAppliedDAO 
+{
+	
+	public boolean applyNew(JobApplied jobApplied);
+	
+	public List<JobApplied> listByUser(String username);
+	
+	public List<JobApplied> listByCompany();
+	
+}
